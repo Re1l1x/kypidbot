ad2a3f4ae01a002c362957b4cd0b226eabcf48dc
jus1d <artfa63@gmail.com>
2026-02-05 22:56:18 +0400

    chore: Update logs for migrate tool

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -3,28 +3,35 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	"log"
+	"log/slog"
+	"os"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 	"github.com/jus1d/kypidbot/internal/config"
+	"github.com/jus1d/kypidbot/internal/lib/logger/sl"
 	"github.com/pressly/goose/v3"
 )
 
 func main() {
+	log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+
 	c := config.MustLoad()
 	p := c.Postgres
 	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=%s", p.User, p.Password, p.Host, p.Port, p.Name, p.ModeSSL)
 	db, err := sql.Open("pgx", dsn)
 	if err != nil {
-		log.Fatalf("open db: %v", err)
+		log.Error("postgresql: failed to connect", sl.Err(err))
+		os.Exit(1)
 	}
 	defer db.Close()
 
 	if err := goose.SetDialect("postgres"); err != nil {
-		log.Fatalf("set dialect: %v", err)
+		log.Error("postgresql: failed to set dialect", sl.Err(err))
+		os.Exit(1)
 	}
 
 	if err := goose.Up(db, "migrations"); err != nil {
-		log.Fatalf("up: %v", err)
+		log.Error("postgresql: failed to apply up migrations", sl.Err(err))
+		os.Exit(1)
 	}
 }
